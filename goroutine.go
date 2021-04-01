0ca31ea09af0fdc033ff9bc13c79189b99984733
jingang <zouzijidelu1>
2021-04-01 19:02:24 +0800

    集合修改为映射

--- goroutine.go
+++ goroutine.go
@@ -1,1 +1,17 @@
 package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func slowFunc(b chan string) {
+	time.Sleep(time.Second * 2)
+	b <- "sleep finished"
+}
+func main() {
+	c := make(chan string)
+	go slowFunc(c)
+	msg := <-c
+	fmt.Printf(msg)
+}
