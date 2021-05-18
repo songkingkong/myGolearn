eacc08c8d7eb00848a6d11c7b2dd3fa3a4919a35
jingang <zouzijidelu1>
2021-05-18 19:19:37 +0800

    for 循环添加注释

--- modellearn/for循环.go
+++ modellearn/for循环.go
@@ -4,9 +4,11 @@ import "fmt"
 
 func main() {
 	m1 := map[string]int{"a": 1, "b": 2}
+	/* for循环内 k 等于下标，v等于值 */
 	for k, v := range m1 {
 		fmt.Println(k, v)
 	}
+	/* for循环内 使用_表示丢弃下标 */
 	for _, v := range m1 {
 		fmt.Println(v)
 	}
