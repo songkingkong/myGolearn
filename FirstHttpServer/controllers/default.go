5ea05f7bc6390b96478533aad7f18cf22fba0c84
jingang <zouzijidelu1>
2021-05-31 13:50:11 +0800

    first http server

--- FirstHttpServer/controllers/default.go
+++ FirstHttpServer/controllers/default.go
@@ -7,9 +7,26 @@ import (
 type MainController struct {
 	beego.Controller
 }
+type UserController struct {
+	beego.Controller
+}
 
 func (c *MainController) Get() {
 	c.Data["Website"] = "beego.me"
 	c.Data["Email"] = "astaxie@gmail.com"
 	c.TplName = "index.tpl"
+}
+
+type User struct {
+	Id       int    `json:"id"`
+	Username string `json:"name"`
+	Phone    string
+}
+
+func (c *UserController) Get() {
+	user := User{1, "tizi365", "13089818901"}
+	c.Ctx.Output.Header("Content-Type", "message/http")
+	c.Ctx.Output.Header("Cache-Control", "no-cache, no-store, must-revalidate")
+	c.Data["xml"] = &user
+	c.ServeXML()
 }
