81eef9f09100f7270ec31543d53a4a4a90f9add2
jingang <zouzijidelu1>
2021-06-01 20:31:24 +0800

    first http server

--- FirstHttpServer/controllers/default.go
+++ FirstHttpServer/controllers/default.go
@@ -27,6 +27,6 @@ func (c *UserController) Get() {
 	user := User{1, "tizi365", "13089818901"}
 	c.Ctx.Output.Header("Content-Type", "message/http")
 	c.Ctx.Output.Header("Cache-Control", "no-cache, no-store, must-revalidate")
-	c.Data["xml"] = &user
-	c.ServeXML()
+	c.Data["json"] = &user
+	c.ServeJSON()
 }
