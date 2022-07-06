de1b3c0a3090e004d53a0f4b01f67ad9c40aae70
jingang.song <jingang.song@funplus.com>
2022-07-06 11:39:14 +0800

    尝试两种单元测试的输出方式

--- syntax/for_test.go
+++ syntax/for_test.go
@@ -9,10 +9,11 @@ func TestFor(t *testing.T) {
 	m1 := map[string]int{"a": 1, "b": 2}
 	/* for循环内 k 等于下标，v等于值 */
 	for k, v := range m1 {
-		fmt.Println(k, v)
+		t.Log(k, v)
 	}
 	/* for循环内 使用_表示丢弃下标 */
 	for _, v := range m1 {
+		t.Log(v)
 		fmt.Println(v)
 	}
 }
